8fe0a90bb453f8ad0b20d9d21c30c40f0c4dcf57
Ruben van der Linde <rubenvdlinde@gmail.com>
2026-03-19 22:34:57 +0100

    feat: add Dutch (nl) locale support for documentation

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,7 +16,11 @@ const config = {
 
   i18n: {
     defaultLocale: 'en',
-    locales: ['en'],
+    locales: ['en', 'nl'],
+    localeConfigs: {
+      en: { label: 'English' },
+      nl: { label: 'Nederlands' },
+    },
   },
 
   presets: [
@@ -58,6 +62,10 @@ const config = {
           {
             href: 'https://github.com/ConductionNL/softwarecatalog',
             label: 'GitHub',
+            position: 'right',
+          },
+          {
+            type: 'localeDropdown',
             position: 'right',
           },
         ],
